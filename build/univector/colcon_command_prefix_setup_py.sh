203b5b6a3b5076d470582f27cbfa6beb0d86f445
Allan  Menchik da Cunha <allan.m@ufms.br>
2024-01-19 15:48:25 +0000

    delete unecessary files

--- build/univector/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/vsss/repos/vsss24-ws/install/univector/share/univector/package.sh"
--- build/univector/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/vsss/repos/vsss24-ws/install/univector'
