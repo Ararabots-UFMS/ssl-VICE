e588058a229527571fec0371e6e3390f51a6d9f4
Menchik <allan.m@ufms.br>
2024-05-23 14:54:57 -0400

    fix: fixed folder search

--- setup_scripts/gui_install_and_update.sh
+++ setup_scripts/gui_install_and_update.sh
@@ -1,17 +1,23 @@
 # Description: Install the GUI dependencies for the VSSS-VICE project
 
-GUI=ssl-gui
-
 # Enter VICE parent directory
-cd ../
+while [ ! -d ./ssl-VICE ]; do
+    cd ..
+    if [ $PWD = "/" ]; then
+        echo "Couldn't find the ssl-VICE directory. Please run this script from the ssl-VICE directory."
+        exit 1
+    fi
+done
 
 # Check if the GUI directory exists, if not, clone the repository and enter the directory
 # else, enter the directory and pull the latest changes
-if test ! -d .$GUI; then
+if test ! -d ./ssl-gui; then
+    echo "Couldn't find the ssl-gui directory. Cloning the repository..."
     git clone https://github.com/Ararabots-UFMS/ssl-gui.git
-    cd $GUI/
-else  
-    cd $GUI/
+    cd ssl-gui/
+else
+    echo "The directory ssl-gui already exists. Pulling the latest changes..."
+    cd ssl-gui/
     git pull
 fi
 
--- src/gui/gui/run_gui.py
+++ src/gui/gui/run_gui.py
@@ -1,12 +1,13 @@
 import subprocess
 import pathlib
 import os
+import warnings
 
 ####################
 # This file was created taking into account that it will be run with
 #   the following command: ros2 run gui run_gui
 # If it is run any other way it probably won't work
-# (Needs to be built with colcon and sourced beforehand)
+# Needs to be built with colcon and sourced beforehand
 ####################
 
 # Set folder names for both repos
@@ -18,21 +19,28 @@ path_to_repo_parent_folder = pathlib.Path(__file__).parent.absolute()
 
 # Find the parent folder to the VICE repo
 while not (path_to_repo_parent_folder / VICE_REPO_NAME).exists():
+
+    # If the path is the root folder, the VICE repo was not found
+    if path_to_repo_parent_folder == pathlib.Path('/'):
+        raise Exception(f"Could not find the {VICE_REPO_NAME} repository folder")
+    
+    # Move to the parent of the current folder
     path_to_repo_parent_folder = path_to_repo_parent_folder.parent
 
-# Add GUI repo name to the path
+# Add GUI repository name to the path
 path_to_gui_repo = path_to_repo_parent_folder / GUI_REPO_NAME
 
 def main():
     # If GUI path does not exist, run the installation script
     if not (path_to_gui_repo).exists():
-        subprocess.run(["bash", "./setup_scripts/gui_install_and_update.sh"])
+        warnings.warn(f"GUI repository not found at {path_to_gui_repo}. Running installation script")
+        subprocess.run(["bash", f"{path_to_repo_parent_folder}/{VICE_REPO_NAME}/setup_scripts/gui_install_and_update.sh"])
 
     # Change directory to GUI repo
     os.chdir(path_to_gui_repo)
 
     # Run the GUI
-    subprocess.run(["npm", "run", "dev"])
+    subprocess.run(["sudo", "npm", "run", "dev"])
 
 
 if __name__ == '__main__':
