eb120262c60cf1dce28919c1b99cd1f734a75171
FabioHuang <fabiohuang2004@icloud.com>
2024-08-14 18:48:42 -0400

    refactor: update communication template

--- src/communication/communication/communication_node.py
+++ src/communication/communication/communication_node.py
@@ -1,30 +1,39 @@
 import rclpy
 from rclpy.node import Node
 
+from typing import List
+
 from communication import feedback_client
 
-from system_interfaces.msg import Communication, Infos
+from system_interfaces.msg import Communication, Infos, Command
 
 class CommunicationNode(Node):
 
     def __init__(self):
         super().__init__('communication_node')
-        self.subscription = self.create_subscription(Communication, 'commandTopic', self.send_command, 10)
+        self.subscription = self.create_subscription(Communication, 'commandTopic', self.update_commands, 10)
 
         # Client to receive robots information.
         self.client = feedback_client()
         self.publisher = self.create_publisher(Infos, 'infoTopic', 10)
         
         # Receiving data at 120 Hz
-        self.timer = self.create_timer(1/120, self.publish_infos)
+        self.info_timer = self.create_timer(1/120, self.publish_infos)
 
-    def send_command(self, msg):
-        # Serialize message and send to hardware.
-        if msg.commands:
-            # Send command to robot
-            pass
+        self.commands: List[Command] = []
+
+        self.command_timer = self.create_timer(1/120, self.publish_command)
+
+    def update_commands(self, msg: List[Command]):
+        self.commands = msg
+        return
+    
+    def publish_command(self, msgs: List[Command]):
+        # Serialize commands and send to robots
+        pass
 
     def publish_infos(self):
+        # Publish 
         pass
 
 def main(args=None):
