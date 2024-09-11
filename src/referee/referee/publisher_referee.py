1f32675f4c78844b8aa1fed754bbcf4757426d8d
Fernando Goncalves <goncalves.fernando@ufms.br>
2024-09-11 13:07:30 -0400

    build: compiled protobuf classes

--- src/referee/referee/publisher_referee.py
+++ src/referee/referee/publisher_referee.py
@@ -1,18 +1,19 @@
 import rclpy
 from rclpy.node import Node
-
-
+from google.protobuf import text_format
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('referee_publisher')
+
         self.declare_parameter('ip', '224.5.23.1')
         self.declare_parameter('port', 10003)
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.client = Client(ip = self.ip, port = self.port)
 
 
     def timer_callback(self):
@@ -24,14 +25,8 @@ class MinimalPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-
     minimal_publisher = MinimalPublisher()
-
     rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
