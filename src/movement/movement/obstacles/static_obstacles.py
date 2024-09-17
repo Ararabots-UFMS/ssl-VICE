d9cbd69ef80914ba401be888b881c1f409f5377f
FabioHuang <fabiohuang2004@icloud.com>
2024-09-17 10:27:41 -0400

    feat: implementing WallObstacle

--- src/movement/movement/obstacles/static_obstacles.py
+++ src/movement/movement/obstacles/static_obstacles.py
@@ -2,6 +2,7 @@ from .interfaces import StaticObstacle
 
 from system_interfaces.msg import VisionGeometry
 
+# This needs to change to use the FieldLineSegment instead of the distances
 class BoundaryObstacles(StaticObstacle):
     def __init__(self, geometry: VisionGeometry):
         self.half_width = geometry.field_width / 2
@@ -11,10 +12,10 @@ class BoundaryObstacles(StaticObstacle):
         if ignore:
             return False
 
-        elif x[0] < -1 * (self.half_length + padding) or x[0] > self.half_length + padding:
+        elif x[0] < -1 * (self.half_length - padding) or x[0] > self.half_length - padding:
             return True
 
-        elif x[1] < -1 * (self.half_width + padding) or x[1] > self.half_width + padding:
+        elif x[1] < -1 * (self.half_width - padding) or x[1] > self.half_width - padding:
             return True
 
         else:
@@ -33,14 +34,36 @@ class BoundaryObstacles(StaticObstacle):
         return np.array([x[0] + (-1 * np.sign(x[0]) * x_distance), x[1] + (-1 * np.sign(x[1]) * y_distance)])
 
 class WallObstacles(StaticObstacle):
-    def __init__(self):
-        pass
-    
+    def __init__(self, geometry: VisionGeometry):
+        self.half_width = geometry.field_width / 2
+        self.half_length = geometry.field_length / 2   
+        self.boundary_width = geometry.boundary_width
+
     def is_colission(self, x: np.matrix, ignore: bool = False, padding: float = 90):
-        pass
+        if ignore:
+            return False
+
+        elif x[0] < -1 * (self.half_length + self.boundary_width - padding) or x[0] > self.half_length + self.boundary_width - padding:
+            return True
+
+        elif x[1] < -1 * (self.half_width + self.boundary_width - padding) or x[1] > self.half_width + self.boundary_width - padding:
+            return True
+
+        else:
+            return False
 
     def closest_outside_point(self, x: np.matrix):
-        pass
+        x_distance = 0
+        y_distance = 0
+        if x[0] < -1 * self.half_length + self.boundary_width or x[0] > self.half_length + self.boundary_width:
+            x_distance  = x[0] - self.half_length - self.boundary_width
+
+        if x[1] < -1 * self.half_width or x[1] > self.half_width:
+            y_distance  = x[1] - self.half_width - self.boundary_width
+
+        # Huge mamaco, there may be a better way...
+        return np.array([x[0] + (-1 * np.sign(x[0]) * x_distance), x[1] + (-1 * np.sign(x[1]) * y_distance)])
+
 
 class FieldObstacles(StaticObstacle):
     def __init__(self):
