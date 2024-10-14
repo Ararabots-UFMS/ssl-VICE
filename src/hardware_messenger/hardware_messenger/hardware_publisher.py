5095086dfad244fafaad542bc01f7b46ff40904a
Menchik <allan.menchik@hotmail.com>
2024-10-14 09:56:01 -0400

    feat: created multiple commands support

--- src/hardware_messenger/hardware_messenger/hardware_publisher.py
+++ src/hardware_messenger/hardware_messenger/hardware_publisher.py
@@ -6,7 +6,7 @@ import struct
 from grsim_messenger.protobuf.grSim_Packet_pb2 import grSim_Packet
 from grsim_messenger.protobuf.grSim_Commands_pb2 import grSim_Robot_Command
 
-from system_interfaces.msg import TeamCommand
+from system_interfaces.msg import TeamCommand, GUIMessage
 from grsim_messenger.inverse_kinematics import apply_inverse_kinematics
 
 
@@ -23,11 +23,36 @@ class HardwarePublisher(Node):
             self.get_parameter("baudrate").get_parameter_value().integer_value
         )
 
+        # Format string tells struct how to pack the data
+        # B: unsigned char (1 byte)
+        # f: float (4 bytes)
+        header_kicker_format = "B"
+        address_format = "BBBBB"
+        velocities_format = "fff"
+        self.robot_command_format = (
+            address_format + header_kicker_format + velocities_format
+        )
+
+        # Commands
+        # 128: only velocities and kicker
+        # 64:
+        # 32:
+        # 16:
+        # 4:
+        # 2:
+        self.command_headers = [128]
+
+        self.header_chooser = -1
+
         self.serial_writer = serial.Serial(self.port, self.baudrate)
         self.serial_writer.open()
 
         self.commandSubscriber = self.create_subscription(
             TeamCommand, "CommandTopic", self.translate_command, 10
+        )
+
+        self.gui_subscriber = self.create_subscription(
+            GUIMessage, "guiTopic", self.gui_callback, 10
         )
 
         self.timer = self._node.create_timer(
@@ -37,38 +62,34 @@ class HardwarePublisher(Node):
     def publish(self):
         self.serial_writer.write(struct.pack(self.format_string, *self._command))
 
+    def gui_callback(self, msg):
+        # self.robot_addresses = msg.addresses
+        pass
+
     # TODO: Add address
     def translate_command(self, command):
-        self._command = [0 for _ in range(5 * len(command.robots))]
+        self._command = []
 
-        # format string tells struct how to pack the data
-        # x: Pad byte (no data)
-        # c: char (1 byte)
-        # b: signed char (1 byte)
-        # B: unsigned char (1 byte)
-        # ?: _Bool (1 byte)
-        # h: short (2 bytes)
-        # H: unsigned short (2 bytes)
-        # i: int (4 bytes)
-        # I: unsigned int (4 bytes)
-        # l: long (4 bytes)
-        # L: unsigned long (4 bytes)
-        # q: long long (8 bytes)
-        # Q: unsigned long long (8 bytes)
-        # f: float (4 bytes)
-        # d: double (8 bytes)
-        # s: char[] (string)
-        # p: char[] (Pascal string)
-        # P: void * (pointer)
-        self.format_string = "Ifff?" * len(command.robots)
+        self.format_string = self.robot_command_format * len(command.robots)
+
+        self.header_chooser += 1
+        if self.header_chooser > len(self.command_headers):
+            self.header_chooser = 0
 
         for robot in command.robots:
-            offset = robot.robot_id * 5
-            self._command[0 + offset] = int(robot.robot_id)
-            self._command[1 + offset] = float(robot.linear_velocity_x)
-            self._command[2 + offset] = float(robot.linear_velocity_y)
-            self._command[3 + offset] = float(robot.angular_velocity)
-            self._command[4 + offset] = int(robot.kick)
+            robot_command = [
+                self.robot_addresses[robot.id][0],
+                self.robot_addresses[robot.id][1],
+                self.robot_addresses[robot.id][2],
+                self.robot_addresses[robot.id][3],
+                self.robot_addresses[robot.id][4],
+                self.command_headers[self.header_chooser] + int(robot.kicker),
+                float(robot.linear_velocity_x),
+                float(robot.linear_velocity_y),
+                float(robot.angular_velocity),
+            ]
+
+            self._command += robot_command
 
 
 def main(args=None):
