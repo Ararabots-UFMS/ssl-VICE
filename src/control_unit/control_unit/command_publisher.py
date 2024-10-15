cb4ae4c5fd05f5c65245918cd72f99b550ef4c9a
Menchik <allan.menchik@hotmail.com>
2024-10-15 11:34:44 -0400

    refactor: Rotate velocities to robot frame in CommandPublisher

--- src/control_unit/control_unit/command_publisher.py
+++ src/control_unit/control_unit/command_publisher.py
@@ -3,6 +3,17 @@ from rclpy.node import Node
 from system_interfaces.msg import TeamCommand, RobotCommand
 from time import time
 
+from math import cos, sin
+
+
+def rotate_velocities_to_robot_frame(vx, vy, orientation):
+    vel_norm = vx * cos(orientation) - vy * sin(orientation)
+    vel_tan = vx * sin(orientation) + vy * cos(orientation)
+    return vel_norm, vel_tan
+
+
+mm_to_m = 1 / 1000
+
 
 class CommandPublisher(Node):
     def __init__(self, coach) -> None:
@@ -23,11 +34,19 @@ class CommandPublisher(Node):
         current_time = time()
         for robot in self.coach.robots.values():
             elapsed_time = current_time - robot.trajectory_start_time
-            _, velocities, _ = robot.trajectory.at_time(elapsed_time)
+
+            position, velocities, _ = robot.trajectory.at_time(elapsed_time)
+
+            vel_norm, vel_tan = rotate_velocities_to_robot_frame(
+                velocities[0] * mm_to_m,
+                velocities[1] * mm_to_m,
+                position[3],
+            )
+
             command = RobotCommand()
             command.robot_id = robot.id
-            command.linear_velocity_x = velocities[0] / 1000
-            command.linear_velocity_y = velocities[1] / 1000
+            command.linear_velocity_x = vel_norm
+            command.linear_velocity_y = vel_tan
             command.angular_velocity = velocities[2]
             msg.robots.append(command)
 
