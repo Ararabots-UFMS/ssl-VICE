b22c0244808298213f5bec36f51282db2a83e7e2
FabioHuang <fabiohuang2004@icloud.com>
2024-10-21 16:39:20 -0400

    fix: fixing not expeted behaviour of dynamic range method

--- src/movement/movement/obstacles/dynamic_obstacles.py
+++ src/movement/movement/obstacles/dynamic_obstacles.py
@@ -3,13 +3,13 @@ from system_interfaces.msg import Robots
 
 from typing import Tuple
 
-from math import sqrt, sin, cos
+from math import sqrt, copysign
 
 from ruckig import InputParameter, OutputParameter, Result, Ruckig, Trajectory, ControlInterface
 
 
-class RoboObstacle(DynamicObstacle):
-    def __init__(self, state: Robots, radius: 90, max_delta: float = 0.5, max_acc = 1, max_vel = 3):
+class RobotObstacle(DynamicObstacle):
+    def __init__(self, state: Robots, radius: float = 90, max_delta: float = 0.5, max_acc = 1, max_vel = 3):
         self.state = state
         self.radius = radius
 
@@ -34,42 +34,53 @@ class RoboObstacle(DynamicObstacle):
         elif delta > self.max_delta:
             delta = self.max_delta
 
-        f_plus, f_minus = self.bb_range(delta)
+        velocity_mag = sqrt(self.state.velocity_x ** 2 + self.state.velocity_y ** 2)
+        # In case the obstacle is stationary, adding a small number to avoid zero division.
+        velocity_mag = 0.0001 if velocity_mag == 0 else velocity_mag
+
+        velocity_norm = self.state.velocity_x / velocity_mag, self.state.velocity_y / velocity_mag
+
+
+        f_plus, f_minus = self.bb_range(delta, velocity_norm)
+
 
         # Distance between f_minus and f_plus...
         dynamic_radius = sqrt((f_plus[0] - f_minus[0]) ** 2 + (f_plus[1] - f_minus[1]) ** 2)
 
-        velocity_mag = (self.state[1][0] ** 2 + self.state[1][1] ** 2)
-        velocity_norm = self.state[1][0] / velocity_mag, self.state[1][1] / velocity_mag
-
-        dynamic_center = self.state[0] + (velocity_norm[0] * (f_minus[0] + dynamic_radius)), self.state[1] + (velocity_norm[1] * (f_minus[1] + dynamic_radius))
+        dynamic_center = self.state.position_x + (velocity_norm[0] * (f_minus[0] + dynamic_radius)), self.state.position_y + (velocity_norm[1] * (f_minus[1] + dynamic_radius))
 
         obs_radius = self.radius + dynamic_radius
 
         return dynamic_center, obs_radius
         
-    def bb_range(self, delta) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    def bb_range(self, delta, velocity_norm: Tuple[float, float]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
         # Get f- and f+ 1d bang bang trajectorys given the delta time.
 
         # F plus
         inp = InputParameter(2)
         inp.control_interface = ControlInterface.Velocity
 
+        inp.max_velocity = [self.max_vel, self.max_vel]
+        inp.max_acceleration = [self.max_acc, self.max_acc]
+
         inp.current_position = [self.state.position_x, self.state.position_y]
         inp.current_velocity = [self.state.velocity_x, self.state.velocity_y]
 
-        inp.target_velocity = [self.max_vel, self.max_vel]
-        inp.target_acceleration = [self.max_acc, self.max_acc]
+        inp.target_velocity = [copysign(self.max_vel, self.state.velocity_x), copysign(self.max_vel, self.state.velocity_y)]
+        inp.target_acceleration = [copysign(self.max_acc, self.state.velocity_x), copysign(self.max_acc, self.state.velocity_y)]
 
         # F minus
         inp2 = InputParameter(2)
         inp2.control_interface = ControlInterface.Velocity
 
+        inp2.max_velocity = [self.max_vel, self.max_vel]
+        inp2.max_acceleration = [self.max_acc, self.max_acc]
+
         inp2.current_position = [self.state.position_x, self.state.position_y]
         inp2.current_velocity = [self.state.velocity_x, self.state.velocity_y]
 
-        inp2.target_velocity = [self.max_vel, self.max_vel]
-        inp2.target_acceleration = [-self.max_acc, -self.max_acc]
+        inp2.target_velocity = [copysign(self.max_vel, -1 * self.state.velocity_x), copysign(self.max_vel, -1 * self.state.velocity_y)]
+        inp2.target_acceleration = [copysign(self.max_acc, -1 * self.state.velocity_x), copysign(self.max_acc, -1 * self.state.velocity_y)]
 
         otg = Ruckig(2)
         f_plus = Trajectory(2)
@@ -81,6 +92,9 @@ class RoboObstacle(DynamicObstacle):
         state_plus = f_plus.at_time(delta)
         state_minus = f_minus.at_time(delta)
 
+        print('oi')
+        print((state_plus[0][0], state_plus[0][0]), (state_minus[0][0], state_minus[0][1]))
+
         # (f+ x, f+ y), (f- x, f- y)
         return ((state_plus[0][0], state_plus[0][0]), (state_minus[0][0], state_minus[0][1]))
 
@@ -88,115 +102,30 @@ class RoboObstacle(DynamicObstacle):
         self.state = state
 
 
-# from movement.obstacles.interfaces import DynamicObstacle
-# from movement.control.steer import integrate_control_2d
+# import matplotlib.pyplot as plt
 
-# import numpy as np
+# fig, ax = plt.subplots()
 
-# # TODO Refactor using ruckig, remove integrate_control_2d and numpy
+# plt.axis([-4500, 4500, -3000, 3000])
 
-# # (x, y, vx, vy) -> ([x ,y , theta], [vx, vy, v_theta])
+# robot = Robots()
 
-# class RobotObstacle(DynamicObstacle):
-#     def __init__(self, x_obs: np.matrix, orientation: float, u_max: float = 1.0, u_min: float = -1.0):
-#         self.x_obs = x_obs
-#         self.orientation = orientation
-#         self.u_max = u_max
-#         self.u_min = u_min
+# robot.id = 0
+# robot.position_x = 0.0
+# robot.position_y = 0.0
+# robot.velocity_x = 200.0
+# robot.velocity_y = -200.0
 
-#     def is_colission(self, x: np.matrix, delta: float, ignore: bool = False, padding: float = 90, use_dynamic: bool = True, max_delta: float = 0.5):
-#         if ignore:
-#             return False
+# deltas = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 
-#         r = padding
+# obs = RobotObstacle(robot, max_delta = 0.5, max_acc = 1000, max_vel = 3000)
 
-#         if use_dynamic:
-#             center, radius = self.get_dynamic_range(delta, max_delta)
-#             distance = np.linalg.norm(self.x_obs[:2] - center)
-#             r += radius
-#         else:
-#             distance = np.linalg.norm(self.x_obs[:2] - x[:2])
-            
-#         if distance < r:
-#             return True
-        
-#         return False
+# for i in deltas:
+#     center, radius = obs.get_dynamic_range(i)
 
-#     def get_dynamic_range(self, delta: float, max_delta: float = 0.5) -> (np.matrix, float):
-#         if delta > max_delta:
-#             delta = max_delta
-        
-#         # Normalizing velocity vector to unit movement direction
-#         direction_vector = self.x_obs[2:] / (np.linalg.norm(self.x_obs[2:] + 0.001))
+#     obstacle_plt = plt.Circle((robot.position_x, robot.position_y), 90, color ='red')
+#     padding_plt = plt.Circle((center), radius, color='red', fill=False)
+#     ax.add_patch( obstacle_plt )
+#     ax.add_patch( padding_plt )
 
-#         direc_acc = [np.sign(self.x_obs[2]) + (self.x_obs[2] == 0), np.sign(self.x_obs[3]) + (self.x_obs[3] == 0)]
-        
-#         can_break, break_time = self.can_break(direc_acc, delta)
-
-#         max_acc = [[[direc_acc[0] * self.u_max, direc_acc[1] * self.u_max], delta]]
-#         min_acc = [[[direc_acc[0] * self.u_min, direc_acc[1] * self.u_min], break_time]]
-
-#         f_plus = np.linalg.norm(self.x_obs[:2] - (integrate_control_2d(self.x_obs, max_acc))[:2])
-
-#         f_minus = np.linalg.norm(self.x_obs[:2] - (integrate_control_2d(self.x_obs, min_acc))[:2])
-
-#         radius = abs(f_plus - f_minus) / 2
-
-#         center = self.x_obs[:2].T + (direction_vector * (f_minus + radius)).transpose()
-        
-#         return center.T, radius
-
-#     def can_break(self, u, delta):
-#         velocity_mag = np.linalg.norm(self.x_obs[2:])
-#         acc_mag = np.linalg.norm(u)
-#         break_time = velocity_mag / acc_mag
-
-#         if break_time > delta:
-#             return False, delta
-
-#         return True, break_time
-
-#     def update_state(self, x_obs: np.matrix, orientation):
-#         self.x_obs = x_obs
-#         self.orientation = orientation
-
-# class BallObstacle(DynamicObstacle):
-#     def __init__(self, x: np.matrix, mode: str):
-#         self.mode = mode
-#         pass
-
-#     def is_colission(self, x: np.matrix, ignore: bool = True, padding: float = 90):
-#         pass
-
-#     def get_dynamic_range(self, delta: float):
-#         pass
-
-#     def update_state(self, x: np.matrix):
-#         pass
-
-#     def set_mode(mode: str):
-#         pass
-
-
-# # import matplotlib.pyplot as plt
-
-# # fig, ax = plt.subplots()
-
-# # plt.axis([0, 4500, 0, 3000])
-
-# # x_obs = np.matrix([[3000], [0], [1000], [0]]).astype(float)
-
-# # max_delta = 0.5
-# # deltas = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-
-# # obs = RobotObstacle(x_obs, 0, 1000, -1000)
-
-# # for i in deltas:
-# #     center, radius = obs.get_dynamic_range(i)
-
-# #     obstacle_plt = plt.Circle((float(x_obs[0]), (float(x_obs[1]))), 90, color ='red')
-# #     padding_plt = plt.Circle(((float(center[0])), (float(center[1]))), radius + 90, color='red', fill=False)
-# #     ax.add_patch( obstacle_plt )
-# #     ax.add_patch( padding_plt )
-
-# # plt.show()
+# plt.show()
