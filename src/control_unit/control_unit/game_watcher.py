6c2a7b1dec253ccb253831b89f30d5f0ec3f6ff6
FabioHuang <fabiohuang2004@icloud.com>
2024-10-22 16:48:18 -0400

    feat: updating game watcher to receive geometry and adding type hints

--- src/control_unit/control_unit/game_watcher.py
+++ src/control_unit/control_unit/game_watcher.py
@@ -1,6 +1,6 @@
 from rclpy.node import Node
 
-from system_interfaces.msg import VisionMessage, GUIMessage, GameData
+from system_interfaces.msg import VisionMessage, GUIMessage, GameData, VisionGeometry
 from strategy.blackboard import Blackboard
 
 
@@ -21,11 +21,18 @@ class GameWatcher(Node):
             GameData, "/referee_messages", self.update_from_gamecontroller, 10
         )
 
-    def update_from_vision(self, message):
+        self.geometry_subscriber = self.create_subscription(
+            VisionGeometry, "geometryTopic", self.update_from_geometry, 10
+        )
+
+    def update_from_vision(self, message: VisionMessage):
         self.blackboard.update_from_vision_message(message)
 
-    def update_from_gamecontroller(self, message):
+    def update_from_gamecontroller(self, message: GameData):
         self.blackboard.update_from_gamecontroller_message(message)
 
-    def update_from_gui(self, message):
+    def update_from_gui(self, message: GUIMessage):
         self.blackboard.update_from_gui_message(message)
+
+    def update_from_geometry(self, message: VisionGeometry):
+        self.blackboard.update_from_geometry(message)
