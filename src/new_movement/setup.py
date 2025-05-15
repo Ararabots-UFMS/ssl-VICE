911fd324c899c932184b4fb211a674b662884c26
Fabio <fabio_huang@ufms.br>
2025-05-15 16:22:00 -0400

    feat: initial commit for new movement

---
+++ src/new_movement/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'new_movement'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='fabio',
+    maintainer_email='fabio_huang@ufms.br',
+    description='This is the movement package for Ararabots SSL, it is responsible for calculating a sequence of points in space given a inital position and a final one.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
