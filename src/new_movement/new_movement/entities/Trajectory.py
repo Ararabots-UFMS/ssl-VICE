7f6b5f20bd1fe4b6b45d07124f94600a826551f5
Fabio <fabio_huang@ufms.br>
2025-06-07 15:41:21 -0400

    feat: adding TrajectorySegment 1

--- src/new_movement/new_movement/entities/Trajectory.py
+++ src/new_movement/new_movement/entities/Trajectory.py
@@ -2,6 +2,7 @@ from States import State, Vector2D, MoveConstraints
 from Motion import MotionPath
 from TrajGenerator import TrajectoryGenerator
 from typing import Optional
+from utils.BB_steer import integrate_control_2d as integrate
 
 
 class TrajectorySegment():
@@ -10,29 +11,56 @@ class TrajectorySegment():
         self.initVel = initVel
         self.motionPath = motionPath
 
+        self.child: Optional['TrajectorySegment'] = None
+
+    def add_child(self, child: 'TrajectorySegment') -> None:
+        ''' Add child segment '''
+        self.child = child
+
     def get_state(self, t) -> State:
+        ''' Get the State at a time t in path '''
         pass
 
     def get_destination(self) -> State:
-        pass
+        ''' Get final destination (Position Vector2D) '''
+        if(self.child == None):
+            # bb_integrate is a State [x, y, vx, vy]            
+            bb_integrate = integrate(self.initPos + self.initVel, self.motionPath.motion_path)
+
+            return State(position = Vector2D(bb_integrate[0], bb_integrate[1]),
+                         velocity = Vector2D(bb_integrate[2], bb_integrate[3]))
+        
+        self.child.get_destination()
 
     def get_total_time(self) -> float:
-        pass
+        ''' Get total time of path '''
+        total_time = 0
+        for p in self.motionPath.motion_path:
+            total_time += p.duration
+
+        if(self.child != None):
+            total_time += self.child.get_total_time()
+
+        return total_time
 
 class Trajectory():
     def __init__(self):
         pass
 
-    def append(self, t) -> None:
+    def append(self) -> None:
+        ''' Append a TrajectorySegment to the end '''
         pass
 
     def connect(self, t) -> None:
+        ''' Connect a TrajectorySegment at a time t '''
         pass
 
     def relocate(self):
+        ''' Append a TrajectorySegment to the beginning '''
         pass
 
     def get_state(self, t) -> State:
+        ''' Get the State at a time t in path '''
         pass
     
     def get_position(self, t) -> Vector2D:
@@ -45,4 +73,5 @@ class Trajectory():
         pass
 
     def get_total_time(selfE) -> float:
+        ''' Get total time of path '''
         pass
