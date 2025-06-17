44161b00a6dee2c80aa5b1fd49d2439c63046340
anniasebold <anniasebold3.0@gmail.com>
2025-06-17 18:49:24 -0400

    refactor: update motion_path type annotation and clean up truncate method

--- src/new_movement/new_movement/entities/Motion.py
+++ src/new_movement/new_movement/entities/Motion.py
@@ -12,7 +12,7 @@ class MotionPrimitive:
 class MotionPath:
     """Piecewise constant acceleration motion path"""
 
-    motion_path: List[MotionPrimitive]
+    motion_path: list[MotionPrimitive]
 
     def truncate(self, t: float) -> None:
         """
@@ -44,18 +44,3 @@ class MotionPath:
                 break
 
         self.motion_path = new_path
-    """Piecewise constant acceleration motion path"""
-
-    motion_path: list[MotionPrimitive]
-
-    def truncate(self, t: float) -> None:
-        """Truncates the motion path by time"""
-        elipsed_time = 0
-        i = 0
-        while self.motion_path[i].duration + elipsed_time < t:
-            elipsed_time += self.motion_path[i].duration
-            i += 1
-
-        self.motion_path[i].duration = t - elipsed_time
-
-        self.motion_path = self.motion_path[0 : i + 1]
