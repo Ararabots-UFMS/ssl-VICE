d982a41ddfd2ee6501d90765e3a5428951ec3929
Fabio <fabio_huang@ufms.br>
2025-07-15 00:48:11 -0400

    feat: implementing field border obstacle constructor and distanceTo methods

--- src/new_movement/new_movement/entities/StaticObstacle.py
+++ src/new_movement/new_movement/entities/StaticObstacle.py
@@ -1,14 +1,59 @@
 from .obstacles import Obstacle, ObstaclePriority
+from .States import Vector2D
+from system_interfaces.msg import VisionGeometry
+
 
 class StaticObstacle(Obstacle):
     def velocity(self) -> float:
         return 0.0
     
 class FieldBorderObstacle(StaticObstacle):
-    pass
+    def __init__(self, geometry: VisionGeometry, padding: float = 90): # 90mm = robots radius
+        self.top_left_point:  Vector2D = None
+        self.top_right_point: Vector2D = None
+        self.bot_left_point:  Vector2D = None
+        self.bot_right_point: Vector2D = None
+    
+        for line in geometry.field_lines:
+            if line.name == 'TopTouchLine':
+                self.top_left_point = Vector2D(line.x1, line.y1) # Not sure if x1 is on the left or right side
+                self.top_right_point = Vector2D(line.x2, line.y2)
+
+            elif line.name == 'BottomTouchLine':
+                self.bot_left_point = Vector2D(line.x1, line.y1)
+                self.bot_right_point = Vector2D(line.x2, line.y2)
+
+    def distanceTo(self, curPosition: Vector2D) -> float:
+        # Searching closest corner
+        closest_corner = None
+        for corner in [self.top_left_point, self.top_right_point, self.bot_left_point, self.bot_right_point]:
+            if closest_corner is None or corner.distance(curPosition) < closest_corner:
+                closest_corner = corner
+
+        if closest_corner.x < closest_corner.y:
+            return abs(curPosition.x - closest_corner.x)
+        else:
+            return abs(curPosition.y - closest_corner.y)
+
+    def isCollidingAt(self, curPosition: Vector2D) -> bool:
+        pass
+
+    def adaptDestination(self, tarPosition: Vector2D) -> Vector2D:
+        pass
+
+    def getPriority(self) -> ObstaclePriority:
+        return ObstaclePriority.HIGHEST
 
 class PenaltyAreaObstacle(StaticObstacle):
-    pass
+    def __init__(self):
+        pass
+    
+    def getPriority(self) -> ObstaclePriority:
+        return ObstaclePriority.LOW
 
 class GenericCircleObstacle(StaticObstacle):
-    pass
+    def __init__(self):
+        pass
+    
+    def getPriority(self) -> ObstaclePriority:
+        return ObstaclePriority.LOWEST
--- src/new_movement/new_movement/entities/obstacles.py
+++ src/new_movement/new_movement/entities/obstacles.py
@@ -8,16 +8,17 @@ class ObstaclePriority():
     HIGH = 30
     MEDIUM = 20
     LOW = 10
+    LOWEST = 0
 
 class Obstacle(ABC):
     @abstractmethod
     def distanceTo(self, curPosition: Vector2D) -> float:
-        ''' Calculate euclidian distance between a position to the obstacle. '''
+        ''' Calculate shortest euclidian distance between a position to the obstacle. '''
         pass
 
     @abstractmethod
-    def isCollidingAt(self, curPosition: Vector2D) -> float:
-        ''' Check if a position collides with the obstacles and returns the collision time '''
+    def isCollidingAt(self, curPosition: Vector2D) -> bool:
+        ''' Check if a position collides with the obstacles and returns the if collision '''
         pass
 
     @abstractmethod
