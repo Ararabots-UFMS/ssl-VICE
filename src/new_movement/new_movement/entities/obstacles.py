7d3053e461803543d4f96fdd6ecd39e1639f43af
Fabio <fabio_huang@ufms.br>
2025-07-14 23:51:07 -0400

    feat: adding abstract obstacle class

---
+++ src/new_movement/new_movement/entities/obstacles.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from .States import Vector2D
+
+class Obstacle(ABC):
+    @abstractmethod
+    def distanceTo(self, curPosition: Vector2D) -> float:
+        ''' Calculate euclidian distance between a position to the obstacle. '''
+        pass
+
+    @abstractmethod
+    def isCollidingAt(self, curPosition: Vector2D) -> float:
+        ''' Check if a position collides with the obstacles and returns the collision time '''
+        pass
+
+    @abstractmethod
+    def adaptDestination(self, tarPosition: Vector2D) -> Vector2D:
+        ''' 
+        If a destination position is in the obstacles, 
+        then return a alternative destination outside obstacle,
+        '''
+        pass
+
+    @abstractmethod
+    def velocity(self) -> float:
+        ''' Returns the velocity of the obstacle '''
+        pass
