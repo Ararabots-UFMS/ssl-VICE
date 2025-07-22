32f9a1d9f832b5128cc455944b64cda755e08d06
Fabio <fabio_huang@ufms.br>
2025-07-22 13:57:04 -0400

    fix: fixing PenaltyAreaObstacle distanceTO method to a cleaner solution

--- src/new_movement/new_movement/entities/StaticObstacle.py
+++ src/new_movement/new_movement/entities/StaticObstacle.py
@@ -18,7 +18,7 @@ class FieldBorderObstacle(StaticObstacle):
         self.top_right_point: Vector2D = None
         self.bot_left_point:  Vector2D = None
         self.bot_right_point: Vector2D = None
-    
+
         for line in geometry.field_lines:
             if line.name == 'TopTouchLine':
                 # TODO Check if padding signal is correct
@@ -32,7 +32,11 @@ class FieldBorderObstacle(StaticObstacle):
     def distanceTo(self, curPosition: Vector2D) -> float:
         closest_corner = self._findClosestCorner(curPosition)
 
-        return min(abs(curPosition.x - closest_corner.x), abs(curPosition.x - closest_corner.x))
+        min_distance_on_axis = min(abs(curPosition.x - closest_corner.x), abs(curPosition.y - closest_corner.y))
+        if(not self.isCollidingAt()):
+            return min_distance_on_axis
+        
+        return min_distance_on_axis
 
     def isCollidingAt(self, curPosition: Vector2D) -> bool:
         if(curPosition.x > self.top_left_point.x and curPosition.x < self.top_right_point):
@@ -65,39 +69,68 @@ class FieldBorderObstacle(StaticObstacle):
         return ObstaclePriority.HIGHEST
 
 class PenaltyAreaObstacle(StaticObstacle):
-    def __init__(self, geometry: VisionGeometry, side: FieldSide):
+    def __init__(self, geometry: VisionGeometry, side: FieldSide, padding: float = 90.0):
+        self.side = side
         self.top_left_point = None
         self.top_right_point = None
         self.bot_left_point = None
         self.bot_right_point = None
+        self.padding = padding
 
-        if side is FieldSide.LEFT:
+        if self.side == FieldSide.LEFT:
             self.top_line = 'LeftFieldRightPenaltyStretch'
             self.bot_line = 'LeftFieldLeftPenaltyStretch'
         else:
             self.top_line = 'RightFieldRightPenaltyStretch'
             self.bot_line = 'RightFieldLeftPenaltyStretch'
-        
+            self.padding = -self.padding  
+
         for line in geometry.field_lines:
             if line.name == self.top_line:
                 #TODO CHECK ORDER OF x and y
-                self.top_left_point = Vector2D()
-                self.top_right_point = Vector2D()
+                self.top_left_point = Vector2D(self.top_line.x1 + self.padding, self.top_line.y1 + self.padding)
+                self.top_right_point = Vector2D(self.top_line.x2 + self.padding, self.top_line.y2 + self.padding)
             if line.name == self.bot_line:
-                self.bot_left_point = Vector2D()
-                self.bot_right_point = Vector2D()
+                self.bot_left_point = Vector2D(self.bot_line.x1 + self.padding, self.bot_line.y1 + self.padding)
+                self.bot_right_point = Vector2D(self.bot_line.x2 + self.padding, self.bot_line.y2 + self.padding)
         
     def distanceTo(self, curPosition: Vector2D) -> float:
-        # TODO
-        pass
+        if(not self.isCollidingAt(curPosition)):
+            # isso ta muito longo, mas se diminuir fica feio kk
+            dx = max(min(self.top_left_point.x, self.top_right_point.x) - curPosition.x, 0 , curPosition.x - max(self.top_left_point.x, self.top_right_point.x))
+            dy = max(min(self.top_left_point.y, self.top_right_point.y) - curPosition.y, 0 , curPosition.y - max(self.top_left_point.y, self.top_right_point.y))
+            distance = Vector2D(dx, dy).size()
+
+        else:
+            closest_coner = self._findClosestCorner(curPosition)
+            distance = min(abs(curPosition.x - closest_coner.x), abs(curPosition.y - closest_coner.y))
+
+        return distance
 
     def isCollidingAt(self, curPosition: Vector2D) -> bool:
-        # TODO
-        pass
+        if self.side == FieldSide.LEFT:
+            if (curPosition.y < self.top_right_point.y and curPosition.y > self.bot_right_point.y) and \
+               (curPosition.x < self.top_left_point.x and curPosition.x > self.bot_left_point.x):
+                return True
+        else:
+            if (curPosition.x > self.top_left_point.x and curPosition.x < self.bot_left_point.x) and \
+               (curPosition.y < self.top_right_point.y and curPosition.y > self.bot_right_point.y):
+                return True
+
+        return False
+
 
     def adaptDestination(self, tarPosition: Vector2D) -> Vector2D:
         # TODO
         pass
+
+    def _findClosestCorner(self, curPosition: Vector2D) -> Vector2D:
+        closest_corner = None
+        for corner in [self.top_left_point, self.top_right_point, self.bot_left_point, self.bot_right_point]:
+            if closest_corner is None or corner.distance(curPosition) < closest_corner:
+                closest_corner = corner
+
+        return closest_corner
 
     def getPriority(self) -> ObstaclePriority:
         return ObstaclePriority.LOW
