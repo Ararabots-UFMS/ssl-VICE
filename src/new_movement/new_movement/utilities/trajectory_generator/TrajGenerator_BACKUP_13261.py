e5b31c8dccbdee706614773ee9fed780ac287496
Fabio Huang <fabio_huang@ufms.br>
2025-08-29 16:44:49 -0400

    updating the driver branch

---
+++ src/new_movement/new_movement/utilities/trajectory_generator/TrajGenerator_BACKUP_13261.py
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+from new_movement.utilities.BB_steer import time_optimal_steer_2d as bb_steer
+=======
+from new_movement.utils.BB_steer import time_optimal_steer_2d as bb_steer
+>>>>>>> dev
+from new_movement.entities.States import State, Vector2D, MoveConstraints
+from new_movement.entities.Motion import MotionPath, MotionPrimitive
+from new_movement.entities.Trajectory import TrajectorySegment
+from typing import Optional
+import numpy as np
+
+
+DEFAULT_VELOCITY_CONSTRAINST = Vector2D(900, 900)
+DEFAULT_ACCELERATION_CONSTRAINST = Vector2D(1900, 1900)
+
+
+class TrajectoryGenerator:
+    def __init__(self, constrainsts: Optional[MoveConstraints] = None):
+        # If constrainsts are not specified, set to default constrainst
+        if constrainsts is not None:
+            self.constrainsts = constrainsts
+        else:
+            self.constrainsts = MoveConstraints(
+                DEFAULT_VELOCITY_CONSTRAINST, DEFAULT_ACCELERATION_CONSTRAINST
+            )
+
+    def generate(self, curState: State, tarState: State) -> TrajectorySegment:
+        """Generates a piecewise constant acceleration motion path using the BB_Steer"""
+
+        bb_output = bb_steer(
+            curState.position + curState.velocity,
+            tarState.position + tarState.velocity,
+            umin=self.constrainsts.min_acceleration,
+            umax=self.constrainsts.max_acceleration,
+        )
+
+        # bb_output is a list of piecewise constant acceleration, is other words, ((ax, ay), d) where d is the duration.
+        motion_path = MotionPath(
+            [MotionPrimitive(Vector2D(out[0][0], out[0][1]), out[1]) for out in bb_output]
+        )
+
+        return TrajectorySegment(curState.position, curState.velocity, motion_path)
