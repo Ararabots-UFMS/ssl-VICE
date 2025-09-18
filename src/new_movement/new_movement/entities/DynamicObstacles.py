93b02c59d1a9e815e192c2d870cdd9a0d06a12ba
Fabio Huang <fabio_huang@ufms.br>
2025-09-18 17:43:05 -0400

    fix: resolving time off set plan with multi robots

--- src/new_movement/new_movement/entities/DynamicObstacles.py
+++ src/new_movement/new_movement/entities/DynamicObstacles.py
@@ -87,13 +87,14 @@ class AllyRobotObstacle(Obstacle):
     '''
     Allies Robots are modeled by their trajectory, in a tube shaped manner.
     '''
-    def __init__(self, robotState: State, trajectory: Trajectory, radius: float = 90):
+    def __init__(self, robotState: State, trajectory: Trajectory, time_offset: float = 0.0, radius: float = 90):
         self.robotState = robotState
         self.trajectory = trajectory
+        self.time_offset = time_offset
         self.radius = radius
 
     def distanceTo(self, curPosition: Vector2D, t: float) -> float:
-        return self.trajectory.get_position(t).distance(curPosition) - self.radius
+        return self.trajectory.get_position(self.time_offset + t).distance(curPosition) - self.radius
     
     def isCollidingAt(self, curPosition: Vector2D, t: float) -> bool:
         if self.distanceTo(curPosition, t) < 0:
