755f1b81d25fe3cd927f9de55a94a89ae23ae9a4
Leoneerd <leonardomarconleal@gmail.com>
2025-10-17 05:26:31 -0400

    feat: new implementation of freekick tatics

--- src/strategy/strategy/tatics/freekick.py
+++ src/strategy/strategy/tatics/freekick.py
@@ -1,50 +1,22 @@
 from new_movement.entities.States import Vector2D
 from system_interfaces.msg._game_state import GameState
 from strategy.skills.skills import Skills
-
+import math
 
 class CenterGoal:
     GOAL_POSITIVE = Vector2D(2250.0, 0.0)
     GOAL_NEGATIVE = Vector2D(-2250.0, 0.0)
 
-class GoalkeeperKickoff:
-    def __init__(self):
-        self.name = "GoalkeeperKickoff"
-        self.skills_factory = Skills("Movement")
-        self.balls = {}
-        self.ball = None
-        self.ally_robots = {}
-        self.create_sub = self.create_subscription(GameState, "game_state", self.game_state_callback, 10)
-        try:
-            self.ball = self.balls[0]
-        except:
-            pass
-        
-    def game_state_callback(self, msg: GameState):
-        self.balls = msg.balls
-        self.ally_robots = msg.ally_robots
-
-    def execute(self, goal_position: Vector2D, angle: float):
-        robot_command = self.skills_factory.move_with_angle(
-            robot_id=0,
-            target_x=goal_position.x,
-            target_y=goal_position.y,
-            vel_x=0.0,
-            vel_y=0.0,
-            angle=angle,
-        )
-        robot_command.field_border = True
-        return robot_command
-
-
 class OurFreekick:
-    def __init__(self, ally_robots, on_positive_half):
+    def __init__(self, ally_robots, balls, on_positive_half):
         self.name = "OurAction"
         self.padding = 500.0  # mm
         self.skills_factory = Skills("Movement")
         self.goal_center = CenterGoal()
         self.on_positive_half = on_positive_half
         self.ally_robots = ally_robots
+        self.balls = balls
+        self.goal_position = self.goal_center.GOAL_NEGATIVE if self.on_positive_half else self.goal_center.GOAL_POSITIVE
 
         if self.on_positive_half:
             self.angle = 3.14159
@@ -61,30 +33,24 @@ class OurFreekick:
             return Vector2D(radius, 0.0)
         return Vector2D(-radius, 0.0)
 
-    def execute(self):
+    def execute(self, ball, goal_position):
         robots_commands = []
-
-        if 0 in self.ally_robots:
-            robots_commands.append(GoalkeeperKickoff().execute(self.gk_target, self.angle))
-
-        field_ids = sorted([rid for rid in self.ally_robots.keys() if rid != 0])
-
-        direction = 1.0 if self.base_pos.x >= 0 else -1.0
+        field_ids = sorted([rid for rid in self.ally_robots.keys()])
 
         for idx, rid in enumerate(field_ids):
-            target_x = self.base_pos.x if idx == 0 else self.base_pos.x + direction * idx * self.padding
-            target_y = self.base_pos.y
-
+            target_x = ball.x + 10 * (ball.x - goal_position.x) / math.sqrt((ball.x - goal_position.x) ** 2 + (ball.y - goal_position.y) ** 2)
+            target_y = ball.y + 10 * (ball.y - goal_position.y) / math.sqrt((ball.x - goal_position.x) ** 2 + (ball.y - goal_position.y) ** 2)
+            angle = math.atan2(goal_position.y - ball.y, goal_position.x - ball.x)
             robot_command = self.skills_factory.move_with_angle(
                 robot_id=rid,
                 target_x=target_x,
                 target_y=target_y,
                 vel_x=0.0,
                 vel_y=0.0,
-                angle=self.angle,
+                angle=angle,
             )
             robot_command.field_border = True
-            robot_command.center_area = True
+            robot_command.ball = True
             robot_command.penalty_area = True
 
             robots_commands.append(robot_command)
