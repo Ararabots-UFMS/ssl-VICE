e77237ddb2b985814942756e02ea76cccc7234bb
Fabio Huang <fabio_huang@ufms.br>
2025-10-18 02:07:13 -0400

    fix: fixing kalman filter giving out of bounds values

--- src/vision/vision/kalman_filter.py
+++ src/vision/vision/kalman_filter.py
@@ -95,86 +95,89 @@ class KalmanFilterClass2D(object):
 
         self.sd_acceleration = acceleration_sd_2d if acceleration_sd_2d else self.sd_acceleration
 
+import numpy as np
+from typing import Optional
+
 class KalmanFilterClass1D(object):
     '''
     Kalman filter for 1D (Angle and Angular velocity).
 
-    Serves for orientation purposes, employing the information in path planning
-    
-    All calculation and code are based on the reference.
-
-    Reference: 
-    - "https://github.com/NickNair/Multiple-Object-Tracking-using-Kalman-Filter"
-    - "https://github.com/LdDl/kalman-filter"
-
-    Complementary documentation:
-
-    - "https://machinelearningspace.com/2d-object-tracking-using-kalman-filter/"
-    - "https://github.com/mabhisharma/Multi-Object-Tracking-with-Kalman-Filter/blob/master/kalmanFilter.py"
-    - "https://cookierobotics.com/071/"
+    Serves for orientation purposes, employing the information in path planning.
+    This version is modified to handle the circular nature of angles in the [-pi, pi] range
+    and uses a numerically stable covariance update to prevent divergence.
     '''
-    def __init__(self, a_sd: float = 0.01, u: float = 0.1, sd_acceleration: float = 1):
+    def __init__(self, a_sd: float = 0.1, u: float = 0.0, sd_acceleration: float = 1.0):
         self.sd_acceleration = sd_acceleration
-
-        #Acceleration
         self.u = u
-
-        #  State vector ; it's [ angle ;  angular velocity]
+        # State vector: [angle; angular velocity]
         self.x = np.matrix([[0], [0]])
-
-        # The matrix that maps state vector to measurement 
+        # Measurement mapping matrix
         self.H = np.matrix([[1, 0]])
-
         # Measurement Covariance
-        self.R = a_sd ** 2
-
-        # The error covariance matrix that is Identity for now. It gets updated based on Q, A and R.
+        self.R = np.matrix([[a_sd ** 2]])
+        # State Covariance matrix
         self.P = np.eye(self.x.shape[0])
         
-    def predict(self, dt):
+    @staticmethod
+    def _wrap_angle(angle: float) -> float:
+        """Wraps an angle to the [-pi, pi] range."""
+        return (angle + np.pi) % (2 * np.pi) - np.pi
+
+    def predict(self, dt: float):
+        # Control input matrix
         self.B = np.matrix([[(dt**2)/2], [dt]]) 
-
-        # The state transition matrix 
-        self.A = np.matrix([[1, dt],
-                            [0, 1]] )
-
-        # Processs Covariance that for our case depends solely on the acceleration
+        # State transition matrix
+        self.A = np.matrix([[1, dt], [0, 1]])
+        # Process Covariance matrix
         self.Q = np.matrix([[(dt**4)/4, (dt**3)/2],
                             [(dt**3)/2, dt**2]]) * self.sd_acceleration**2
 
-        # If we add the B.u it doesnt work... maybe its a dt problem
+        # Predict the next state
         self.x = np.dot(self.A, self.x) + np.dot(self.B, self.u)
         
-        # Updation of the error covariance matrix 
+        # Wrap the predicted angle to the [-pi, pi] range
+        self.x[0, 0] = self._wrap_angle(self.x[0, 0])
+
+        # Update the state covariance matrix
         self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
 
         return self.x
 
     def update(self, z: np.matrix):
-        # z is the measurement taken, it should be a matrix [[angle]] measured.
-        # Two sources use different operators between HPH^T and R. One uses + and other -
-        # Innovation covariance matrix
+        # z is the measurement taken, should be a matrix [[angle]]
+        
+        # Calculate the residual (innovation) and wrap it
+        predicted_measurement = np.dot(self.H, self.x)
+        residual = z - predicted_measurement
+        residual[0, 0] = self._wrap_angle(residual[0, 0])
+
+        # Innovation covariance
         S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
 
         # Kalman Gain
-        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S)) 
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
 
-        # Update State vector
-        self.x = self.x + np.dot(K, (z - np.dot(self.H, self.x)))
+        # Update the state vector using the wrapped residual
+        self.x = self.x + np.dot(K, residual)
+        
+        # Wrap the updated angle state to keep it in range
+        self.x[0, 0] = self._wrap_angle(self.x[0, 0])
 
+        # --- MODIFICATION START: STABLE COVARIANCE UPDATE ---
+        # Update the state covariance using the numerically stable Joseph form
         I = np.eye(self.H.shape[1])
-
-        self.P = (I -(K*self.H))*self.P  
+        I_KH = I - np.dot(K, self.H)
+        self.P = np.dot(I_KH, self.P).dot(I_KH.T) + np.dot(K, self.R).dot(K.T)
+        # --- MODIFICATION END ---
         
         return self.x
 
     def set_param(self, a_sd: Optional[float] = None,
                         u_a:  Optional[float] = None,
                         acceleration_sd_1d: Optional[float] = None):
-
-        self.R = a_sd ** 2 if a_sd else self.R
-        
-        self.u = u_a if u_a else self.u
-
-        self.sd_acceleration = acceleration_sd_1d if acceleration_sd_1d else self.sd_acceleration
-        
+        if a_sd is not None:
+            self.R = np.matrix([[a_sd ** 2]])
+        if u_a is not None:
+            self.u = u_a
+        if acceleration_sd_1d is not None:
+            self.sd_acceleration = acceleration_sd_1d
