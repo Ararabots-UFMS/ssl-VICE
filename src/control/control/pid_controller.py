aab245e6b3fc5c0cfa4d4711644c549c2e202d01
Fabio Huang <fabio_huang@ufms.br>
2025-10-18 01:19:56 -0400

    paunocudequemtalendo2

--- src/control/control/pid_controller.py
+++ src/control/control/pid_controller.py
@@ -14,8 +14,8 @@ class PIDController:
         self.integral: float = 0.0
         self.previous_time: Optional[float] = None
 
-        self.integral_limit: float = 0.03  # Anti Windup
-        self.output_limit: float = 0.1  # Max velocity
+        self.integral_limit: float = 0.3  # Anti Windup
+        self.output_limit: float = 0.3  # Max velocity
 
     def update_params(self, kp: float, ki: float, kd: float):
         self.kp = kp
