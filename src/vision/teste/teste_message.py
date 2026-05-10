4806e689ad5b924a778b4da40277a951d70bc23b
GabrielF <gabrielfariaferreira19@gmail.com>
2026-05-09 22:26:51 -0400

    seila

--- src/vision/teste/teste_message.py
+++ src/vision/teste/teste_message.py
@@ -2,22 +2,12 @@ import pytest
 from vision.world_message import wrap_message
 from system_interfaces.msg import VisionMessage
 
-def test_invalid_values_are_discarded():
-    # Simula objetos com valores absurdos
-    objects = {
-        "robot1": type("Obj", (), {"position_x": float("nan"), "position_y": 100})(),
-        "ball1": type("Obj", (), {"position_x": 999999, "position_y": 999999})(),
-    }
-    msg = wrap_message(objects)
-    # Nenhum objeto inválido deve ser adicionado
-    assert len(msg.yellow_robots) == 0
-    assert len(msg.balls) == 0
+def test_invalid_values_arediscarded():
+    id = ID(id=0, isball=True)
+    obj = Object([[999999.0], [999999.0]], id, confidence=1.0)
+    obj.update(999999.0, 999999.0, 1.0)
+    objects = {id_: obj}
 
-def test_valid_values_are_added():
-    objects = {
-        "robot1": type("Obj", (), {"position_x": 100, "position_y": 200, "id": type("ID", (), {"is_ball": False})()})(),
-        "ball1": type("Obj", (), {"position_x": 50, "position_y": 60, "id": type("ID", (), {"is_ball": True})()})(),
-    }
+
     msg = wrap_message(objects)
-    assert len(msg.yellow_robots) == 1
-    assert len(msg.balls) == 1
+    assert len(msg.balls) == 0
